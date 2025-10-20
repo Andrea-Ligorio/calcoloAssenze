70b1c8106ce6b869c658478b8a98d1b5442fd1e9
Andrea-Ligorio <andreorio14@gmail.com>
2025-10-20 20:55:23 +0200

    0.1.2

--- calcoloAssenze.cpp
+++ calcoloAssenze.cpp
@@ -11,40 +11,42 @@ using namespace std;
 
 string week(int i);
 
-void weekSetting(int difH[],int& Q,int& wlyH);
+void weekSetting(int difH[], int& Q, int& wlyH);
 void schoolSetting(float& H);
 void setAbsence(float& H);
+
+
 
 int main(){
 	const float absPerc=25;//percentuale di assenze su le ore totali che si possono fare
 	
-	float totH=0;//ore totali da fare in un anno
-	float absH=0;//ore di assenza fatte fino ad ora
+	float totH = 0;//ore totali da fare in un anno
+	float absH = 0;//ore di assenza fatte fino ad ora
 	
-	int weekQ=0;//quantita di ore giornaliere diverse
+	int weekQ = 0;//quantita di ore giornaliere diverse
 	int dayH[7];//array che salva ogni quantita giornaliera di ore diversa
-	int weeklyH=0;//ore settimanali
+	int weeklyH = 0;//ore settimanali
 	
 	bool used;//stato che ci fa capire se i dati sono gia stati settati
 	
 	int i;
 	
 	fstream myfile;
-	myfile.open("assenze.txt",ios::in);
+	myfile.open("assenze.txt", ios :: in);
 
 	if(!myfile.is_open()){
-		cout<<"Errore di apertura file in input"<<endl;
+		cout << "Errore di apertura file in input" << endl;
 		return 127;
 	}
 
-	myfile>>used;
+	myfile >> used;
 	if(used){
-		myfile>>totH;
-		myfile>>weeklyH;
-		myfile>>weekQ;
-		for(i=0;i<weekQ;i++)
-			myfile>>dayH[i];
-		myfile>>absH;	
+		myfile >> totH;
+		myfile >> weeklyH;
+		myfile >> weekQ;
+		for(i = 0 ; i < weekQ ; i++)
+			myfile >> dayH[i];
+		myfile >> absH;	
 	}
 
 	
@@ -64,26 +66,26 @@ int main(){
 			used=true;
 		}
 		
-		posAbsH=(totH*(absPerc/100))-absH;
+		posAbsH = (totH*(absPerc / 100)) - absH;
 		
-		cout<<"Puoi ancora assentarti:"<<endl;
-		cout<<posAbsH<<" ore"<<endl;
+		cout<<"Puoi ancora assentarti:" << endl;
+		cout<<posAbsH<<" ore" << endl;
 		for(i=0;i<weekQ;i++){
-			cout<<posAbsH/dayH[i]<<" giorni da "<<dayH[i]<<" ore"<<endl;
+			cout<<posAbsH/dayH[i] << " giorni da " << dayH[i] << " ore" << endl;
 		}
-		cout<<posAbsH/weeklyH<<" settimane"<<"\n\n";
+		cout << posAbsH / weeklyH << " settimane" << "\n\n";
 		
-		cin>>command;
+		cin >> command;
 		
 		switch (command){
 			case 'h'://help
 				system("cls");
 				
-				cout<<"- w -> setta le ore di ogni giorno della settimana"<<endl;
-				cout<<"- s -> setta le ore totali dell'anno scolastico"<<endl;
-				cout<<"- a -> setta le ore di assenza effettuate fino ad ora"<<endl;
-				cout<<"- e -> chiude il programma salvando i dati inseriti"<<endl;
-				cout<<"- r -> resetta i dati e chiude il programma"<<"\n\n";
+				cout << "- w -> setta le ore di ogni giorno della settimana" << endl;
+				cout << "- s -> setta le ore totali dell'anno scolastico" << endl;
+				cout << "- a -> setta le ore di assenza effettuate fino ad ora" << endl;
+				cout << "- e -> chiude il programma salvando i dati inseriti" << endl;
+				cout << "- r -> resetta i dati e chiude il programma" << "\n\n";
 				break;
 			case 'w'://week
 				weekSetting(dayH,weekQ,weeklyH);
@@ -95,10 +97,10 @@ int main(){
 				setAbsence(absH);
 				break;
 			case 'e'://exit
-				cout<<"salvataggio"<<endl;
+				cout << "salvataggio" << endl;
 				break;
 			case 'r'://reset
-				cout<<"Sicuro di voler resettare tutti i parametri? [y] [n]:"<<endl;
+				cout << "Sicuro di voler resettare tutti i parametri? [y] [n]:" << endl;
 				cin>>command;
 				break;
 			default:
@@ -115,37 +117,37 @@ int main(){
 		return 127;
 	}
 	
-	myfile<<used<<endl;
-	myfile<<totH<<endl;
-	myfile<<weeklyH<<endl;
-	myfile<<weekQ<<endl;
-	for(i=0;i<weekQ;i++)
-		myfile<<dayH[i]<<endl;
-	myfile<<absH;
+	myfile << used << endl;
+	myfile << totH << endl;
+	myfile << weeklyH << endl;
+	myfile << weekQ << endl;
+	for(i = 0 ; i < weekQ ; i++)
+		myfile << dayH[i]<<endl;
+	myfile << absH;
 	
 	myfile.close();
 	
 return 0;
 }
 
-void weekSetting(int difH[],int& Q,int& wlyH){
-	int i,j,H;
+void weekSetting(int difH[], int& Q, int& wlyH){
+	int i, j, H;
 	int dayWeek;
 	bool flag;
 	
 	system("cls");
 	
-	cout<<"Inserisci quanti giorni a settimana vai a scuola:"<<"\n\n";
+	cout << "Inserisci quanti giorni a settimana vai a scuola:" << "\n\n";
 	do{
-		cin>>dayWeek;	
-	}while(dayWeek<1||dayWeek>7);
+		cin >> dayWeek;	
+	}while(dayWeek < 1 || dayWeek > 7);
 	
 	
-	for(i=0;i<dayWeek;i++){
-		cout<<"Inserisci quante ore fai di "<<week(i)<<": "<<"\n\n";
+	for(i = 0 ; i < dayWeek ; i++){
+		cout << "Inserisci quante ore fai di " << week(i) << ": " << "\n\n";
 		do{
-			cin>>H;	
-		}while(H<1||H>24);
+			cin >> H;	
+		}while(H < 1 || H > 24);
 		
 		wlyH+=H;
 		
@@ -155,8 +157,8 @@ void weekSetting(int difH[],int& Q,int& wlyH){
 			}
 		}
 		
-		if(!flag||i==0){
-			difH[i]=H;
+		if(!flag || i == 0){
+			difH[i] = H;
 			Q++;	
 		}
 	}
@@ -165,36 +167,36 @@ return;
 
 void schoolSetting(float& H){
 	
-	const int n=4;
-	string schName[n]{"Tecnico","Scientifico","Professionale","Personalizzato"};
-	int schH[n]{1056,990,1122,0};
-	int i,opt;
+	const int n = 4;
+	string schName[n]{"Tecnico", "Scientifico", "Professionale", "Personalizzato"};
+	int schH[n]{1056, 990, 1122, 0};
+	int i, opt;
 	
 	system("cls");
 	
 	cout<<"Inserisci la scuola che frequenti:"<<endl;
 	
 	for(i=0;i<n;i++){
-		cout<<i+1<<" - "<<schName[i]<<" ("<<schH[i]<<")"<<endl;
+		cout << i + 1 << " - " << schName[i] << " (" << schH[i] << ")" << endl;
 	}
-	cout<<"\n\n";
+	cout << "\n\n";
 	do{
-		cin>>opt;
-	}while(opt<1||opt>n);
+		cin >> opt;
+	}while(opt < 1 || opt > n);
 	
 	
 	
 	opt--;
 	
-	if(opt==n-1){
-		cout<<"Inserisci le ore annuali totali:"<<endl;
+	if(opt == n - 1){
+		cout << "Inserisci le ore annuali totali:" << endl;
 		
 		do{
-			cin>>schH[opt];
-		}while(schH[opt]<2);
+			cin >> schH[opt];
+		}while(schH[opt] < 2);
 	}
 	
-	H=schH[opt];
+	H = schH[opt];
 	
 	system("cls");
 	
